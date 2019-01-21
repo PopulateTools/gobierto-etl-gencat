f7df2e5d1b22bfaabbbb1afc316a2a782c2bd166
Fernando Blat <ferblape@gmail.com>
2019-01-21 15:43:35 +0100

    Update mappings

--- utils/trips_importer.rb
+++ utils/trips_importer.rb
@@ -17,43 +17,43 @@ module Utils
         puts "Processing Row... #{ row.pretty_inspect }\n\n"
         person = @people_importer.import!(
           attributes: {
-            name: row.cleaned_text("Nom i cognoms"),
-            position: row.cleaned_text("Càrrec")
+            name: row.cleaned_text("nom_i_cognoms"),
+            position: row.cleaned_text("c_rrec")
           }
         )
         department = @department_importer.import!(
           attributes: {
-            name: row.cleaned_text("Departament")
+            name: row.cleaned_text("departament")
           }
         )
 
         not_persisted_resources = [person, department].select { |resource| resource.new_record? }
         if not_persisted_resources.blank?
-          trips_with_destinations = GobiertoPeople::Invitation.where('meta @> ?', { original_destinations_attribute: row.cleaned_text("Destinació") }.to_json)
+          trips_with_destinations = GobiertoPeople::Invitation.where('meta @> ?', { original_destinations_attribute: row.cleaned_text("destinaci") }.to_json)
           destinations = if trips_with_destinations.exists?
                            trips_with_destinations.first.destinations_meta
                          else
-                           row.locations_list("Destinació")
+                           row.locations_list("destinació")
                          end
           @trip_importer.import!(
-            attributes: { external_id: row["Id"] },
+            attributes: { external_id: row[":id"] },
             extra: { person_id: person.id,
-                     title: row.cleaned_text("Motiu"),
-                     description: row.cleaned_text("Agenda"),
-                     start_date: row.datetime("Inici viatge", fallback: row.datetime("Fi viatge")),
-                     end_date: row.datetime("Fi viatge", fallback: row.datetime("Inici viatge")),
+                     title: row.cleaned_text("motiu"),
+                     description: row.cleaned_text("agenda"),
+                     start_date: row.datetime("inici_viatge", fallback: row.datetime("fi_viatge")),
+                     end_date: row.datetime("fi_viatge", fallback: row.datetime("inici_viatge")),
                      destinations_meta: destinations,
                      department_id: department.id,
-                     meta: { "purpose" => row.raw_text("Motiu"),
-                             "company" => row.raw_text("Comitiva"),
-                             "food_expenses" => row.economic_amount("Dietes y manutenció"),
-                             "accomodation_expenses" => row.economic_amount("Allotjament"),
-                             "transport_expenses" => row.economic_amount("Transport"),
-                             "other_expenses" => row.economic_amount("Altres despeses"),
-                             "total_expenses" => row.economic_amount("Total despeses"),
-                             "other_expenses_details" => row.raw_text("Especificació altres despeses"),
-                             "comments" => row.raw_text("Observacions"),
-                             "original_destinations_attribute" => row.cleaned_text("Destinació") } }
+                     meta: { "purpose" => row.raw_text("motiu"),
+                             "company" => row.raw_text("comitiva"),
+                             "food_expenses" => row.economic_amount("dietes_i_manutenci"),
+                             "accomodation_expenses" => row.economic_amount("allotjament"),
+                             "transport_expenses" => row.economic_amount("transport"),
+                             "other_expenses" => row.economic_amount("altres_despeses"),
+                             "total_expenses" => row.economic_amount("total_despeses"),
+                             "other_expenses_details" => row.raw_text("especificaci_altres_despeses"),
+                             "comments" => row.raw_text("observacions"),
+                             "original_destinations_attribute" => row.cleaned_text("destinaci") } }
           )
         else
           puts "Some associated resources couldn't be resolved, preventing the creation of trip with external_id: #{ row[":id"] }"
