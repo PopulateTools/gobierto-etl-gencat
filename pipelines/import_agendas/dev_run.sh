d8217e5bcab4ea60c71194b57387897ff29090f7
Fernando Blat <ferblape@gmail.com>
2019-01-23 11:46:51 +0100

    Export trips to CARTO

--- pipelines/import_agendas/dev_run.sh
+++ pipelines/import_agendas/dev_run.sh
@@ -3,9 +3,10 @@
 GENCAT_SITE_DOMAIN="gencat.gobierto.test"
 CLEAR_PREVIOUS_DATA="True"
 RAILS_ENV="development"
+WORKING_DIR=/tmp/gencat
 
 # Extract > Download last start query date
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download-s3/run.rb "gencat/gobierto_people/last_execution/last_start_query_date-$RAILS_ENV.txt" /tmp/gencat/downloads/
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download-s3/run.rb "gencat/gobierto_people/last_execution/last_start_query_date-$RAILS_ENV.txt" $WORKING_DIR/downloads/
 
 # cd $DEV_DIR/gobierto-etl-gencat/; ruby operations/gobierto_people/clear-path/run.rb downloads/last_start_query_date-$RAILS_ENV.txt
 # Extract > Clear previous data
@@ -26,13 +27,17 @@ cd $DEV_DIR/gobierto-etl-gencat/; ruby operations/gobierto_people/clear-path/run
 # Extract > Download data
 while read args; do
   cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download/run.rb $args
-done < /tmp/gencat/datasets_for_extraction
+done < $WORKING_DIR/datasets_for_extraction
 
 # Transform & Load > Process resources
 cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-gencat/operations/gobierto_people/import-gifts/run.rb downloads/datasets/gifts.csv $GENCAT_SITE_DOMAIN
 cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-gencat/operations/gobierto_people/import-invitations/run.rb downloads/datasets/invitations.csv $GENCAT_SITE_DOMAIN
 cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-gencat/operations/gobierto_people/import-trips/run.rb downloads/datasets/trips.csv $GENCAT_SITE_DOMAIN
 cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-gencat/operations/gobierto_people/import-events/run.rb downloads/datasets/events.csv $GENCAT_SITE_DOMAIN
+
+# Upload trips to S3 for CARTO
+cd $DEV_DIR/gobierto; bin/rails runner $DEV_DIR/gobierto-etl-gencat/operations/gobierto_people/export-trips/run.rb $GENCAT_SITE_DOMAIN gencat_trips_$RAILS_ENV.csv
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/upload-s3/run.rb $WORKING_DIR/gencat_trips_$RAILS_ENV.csv gencat/gobierto_people/gencat_trips_$RAILS_ENV.csv
 
 # Documentation > Upload last execution date
 cd $DEV_DIR/gobierto-etl-utils/; ruby operations/upload-s3/run.rb /tmp/gencat/output/start_query_date.txt "gencat/gobierto_people/last_execution/last_start_query_date-$RAILS_ENV.txt"
